468a3a3532173bf47621e6ba53e40a5c82f99e75
jonas.v.bjerke@gmail.com <jonas.v.bjerke@gmail.com>
2016-09-04 14:05:19 +0200

    Init

---
+++ src/webparts/spFxPropertiesDatePicker/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ISpFxPropertiesDatePickerStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'spFxPropertiesDatePickerStrings' {
+  const strings: ISpFxPropertiesDatePickerStrings;
+  export = strings;
+}
---
+++ src/webparts/spFxPropertiesDatePicker/tests/SpFxPropertiesDatePicker.test.ts
@@ -0,0 +1,7 @@
+import * as assert from 'assert';
+
+describe('SpFxPropertiesDatePickerWebPart', () => {
+  it('should do something', () => {
+    assert.ok(true);
+  });
+});
